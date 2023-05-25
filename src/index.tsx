4f72e4cc573d2f0c6a93a301c70ca3d649ba6a99
Julie Brown <juliebrown26@gmail.com>
2023-05-25 13:28:54 -0600

    Add initial iframe and error handling

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
 
@@ -25,6 +25,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -36,21 +38,50 @@ const App = () => {
       },
     });
 
-    // console.log(result);
+    // setCode(result.outputFiles[0].text);
 
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head>
+        <body>
+          <div id="root"></div>
+          <script>
+            window.addEventListener('message', (event) => {
+              try {
+                eval(event.data);
+              } catch (err) {
+                const root = document.querySelector('#root');
+                root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                console.error(err);
+              }
+              
+            }, false);
+          </script>
+        </body>
+      </head>
+    </html>
+  `;
 
   return (
     <div>
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        rows={15}
+        cols={50}
       ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        sandbox="allow-scripts"
+        srcDoc={html}
+        title="preview"
+        ref={iframe}
+      />
     </div>
   );
 };
